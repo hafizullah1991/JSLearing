7f75dae209ef59b2ff9478fb0cad693c70a96f9d
hafizullah1991 <Ahmadshakhi2020@gmail.com>
2025-04-04 21:51:59 -0700

    Learning Js

---
+++ main.js
@@ -0,0 +1,20 @@
+// function alertTest() {
+//   console.log("this is function");
+// }
+// const greeting = document.getElementById('greeting').innerHTML = "Goodbye";
+// alertTest();
+// alertTest();
+// function testing(message) {
+//   console.log(message)
+// }
+
+// testing("computer");
+// testing("good bye");
+
+const localTime = document.getElementByClassName("local-time")[0];
+
+// // const localTime = document.getElementsByClassName("time-number");
+// // for(let i =0; i<localTime.length; i++){
+// //   localTime[i].innerHTML = '10';
+// }
+document.querySelector
