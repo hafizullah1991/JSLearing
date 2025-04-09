82a6d7f0f83c5727c8f2e397f94b0c6e65686ad0
hafizullah1991 <Ahmadshakhi2020@gmail.com>
2025-04-09 09:56:20 -0700

    continue to learn

--- main.js
+++ main.js
@@ -11,10 +11,32 @@
 // testing("computer");
 // testing("good bye");
 
-const localTime = document.getElementByClassName("local-time")[0];
+// const localTime = document.getElementByClassName("local-time")[0];
 
 // // const localTime = document.getElementsByClassName("time-number");
 // // for(let i =0; i<localTime.length; i++){
 // //   localTime[i].innerHTML = '10';
 // }
-document.querySelector
+//  adding CSS style using DOM ==========.style.backgroundColor or fontSize...
+// const greeting = document.getElementById('greeting').style.backgroundColor = 'Green';
+// const weather = document.querySelector("p#weather").style.color = 'red';
+
+// adding event listener for DOM ( document object Model)
+const greetingPost = document.getElementById('greeting').addEventListener('click', function(){
+  this.style.color = 'green', this.style.marginLeft = '123px';
+})
+
+console.log("----------creating HamBuger Menu using JavaScript=---------------")
+
+document.querySelector('#open-nav-menu').addEventListener('click', function(){
+document.querySelector('header nav .wrapper').classList.add('nav-open');
+})
+document.querySelector('#close-nav-menu').addEventListener('click', function(){
+  document.querySelector('header nav .wrapper').classList.remove('nav-open');
+  var name = "kkkkkkk";
+})
+
+console.log("Different other examples so I can learn")
+console.log("practice Var Let and ...Varialbles");
+
+
